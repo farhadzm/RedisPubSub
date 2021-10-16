7f015df913bbda8b901a932f61eefc1e9462c45c
farhad zamani <farhad.zamani7720@gmail.com>
2021-10-16 23:05:45 +0330

    [feat] adding Serilog into project

---
+++ RedisPubSub/Extensions/ProgramExtension.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.Configuration;
+using Serilog;
+using Serilog.Filters;
+using Serilog.Formatting.Elasticsearch;
+using Serilog.Formatting.Json;
+using Serilog.Sinks.Elasticsearch;
+using Serilog.Sinks.RollingFile;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RedisPubSub.Extensions
+{
+    public static class ProgramExtensions
+    {
+        public static void ConfigureLogging()
+        {
+            var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .Enrich.WithMachineName()
+                .Filter.ByExcluding(Matching.FromSource("System"))
+                .Filter.ByExcluding(Matching.FromSource("Microsoft"))
+                .WriteTo.Console()
+                .Enrich.WithProperty("Environment", environment)
+                .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(configuration["ElasticConfiguration:Uri"].ToString()))
+                {
+                    FailureCallback = e => Console.WriteLine("Unable to submit event " + e.MessageTemplate),
+                    EmitEventFailure = EmitEventFailureHandling.WriteToSelfLog |
+                                       EmitEventFailureHandling.WriteToFailureSink |
+                                       EmitEventFailureHandling.RaiseCallback,
+                    FailureSink = new RollingFileSink("./failures.txt", new JsonFormatter(), null, null),
+                    IndexFormat = $"{configuration["ElasticConfiguration:IndexFormat"]}-{DateTime.UtcNow:yyyy-MM}",
+                    CustomFormatter = new ElasticsearchJsonFormatter(),
+                    AutoRegisterTemplate = true
+                })
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+        }
+    }
+}
--- RedisPubSub/Program.cs
+++ RedisPubSub/Program.cs
@@ -2,9 +2,12 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using RedisPubSub.Extensions;
+using Serilog;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace RedisPubSub
@@ -13,15 +16,23 @@ namespace RedisPubSub
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            ProgramExtensions.ConfigureLogging();
+            try
+            {
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (System.Exception ex)
+            {
+                Log.Fatal($"Failed to start {Assembly.GetExecutingAssembly().GetName().Name}", ex);
+                throw;
+            }
         }
-
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                    webBuilder.UseUrls(args);
-                });
+                })
+                .UseSerilog();
     }
 }
